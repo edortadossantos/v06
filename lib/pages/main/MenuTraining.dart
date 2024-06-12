f167398119ffa0db386ee879565ea287afe11cba
Edorta <edortadossantos@gmail.com>
2024-06-12 18:22:07 +0200

    MenuTrainingv1

--- lib/pages/main/MenuTraining.dart
+++ lib/pages/main/MenuTraining.dart
@@ -1,14 +1,325 @@
+import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
+import 'package:menu_navigator/theme/colors.dart';
 
-
-class MenuTraining extends StatelessWidget {
+class MenuTraining extends StatefulWidget {
   const MenuTraining({super.key});
 
   @override
+  State<MenuTraining> createState() => _MenuTrainingState();
+}
+
+class _MenuTrainingState extends State<MenuTraining> {
+  @override
   Widget build(BuildContext context) {
+    var media = MediaQuery.of(context).size;
     return Container(
-      color: Colors.brown,
-      child: const Text('Index 1: Entrenamientos'),
+      decoration:
+          BoxDecoration(gradient: LinearGradient(colors: [primary, secondary])),
+      child: NestedScrollView(
+          headerSliverBuilder: (context, innerboxisscrolled) {
+            return [
+              // Titulo
+              SliverAppBar(
+                backgroundColor: Colors.transparent,
+                centerTitle: true,
+                elevation: 0,
+                pinned: true,
+                leadingWidth: 0,
+                title: Text(
+                  "Seguimiento del entrenamiento",
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 16,
+                      fontWeight: FontWeight.w700),
+                ),
+                // Botón tres puntos
+                actions: [
+                  Padding(
+                    padding: const EdgeInsets.only(right: 16.0),
+                    child: IconButton(
+                        onPressed: () {},
+                        icon: Container(
+                          width: 35,
+                          height: 35,
+                          decoration: BoxDecoration(
+                              color: Colors.black.withOpacity(0.03),
+                              borderRadius: BorderRadius.circular(5)),
+                          child: Center(
+                            child: Icon(
+                              Icons.more_horiz,
+                              size: 22,
+                              color: Colors.black.withOpacity(0.3),
+                            ),
+                          ),
+                        )),
+                  ),
+                ],
+              ),
+
+              SliverAppBar(
+                backgroundColor: Colors.transparent,
+                centerTitle: true,
+                elevation: 0,
+                leadingWidth: 0,
+                expandedHeight: media.width * 0.5,
+                flexibleSpace:
+                    // Grafica
+                    Container(
+                        padding: const EdgeInsets.symmetric(horizontal: 20),
+                        height: media.width * 0.5,
+                        width: double.maxFinite,
+                        child: LineChart(
+                          LineChartData(
+                            lineTouchData: LineTouchData(
+                              enabled: true,
+                              handleBuiltInTouches: false,
+                              touchCallback: (FlTouchEvent event,
+                                  LineTouchResponse? response) {
+                                if (response == null ||
+                                    response.lineBarSpots == null) {
+                                  return;
+                                }
+                              },
+                              mouseCursorResolver: (FlTouchEvent event,
+                                  LineTouchResponse? response) {
+                                if (response == null ||
+                                    response.lineBarSpots == null) {
+                                  return SystemMouseCursors.basic;
+                                }
+                                return SystemMouseCursors.click;
+                              },
+                              getTouchedSpotIndicator:
+                                  (LineChartBarData barData,
+                                      List<int> spotIndexes) {
+                                return spotIndexes.map((index) {
+                                  return TouchedSpotIndicatorData(
+                                    FlLine(
+                                      color: Colors.transparent,
+                                    ),
+                                    FlDotData(
+                                      show: true,
+                                      getDotPainter:
+                                          (spot, percent, barData, index) =>
+                                              FlDotCirclePainter(
+                                        radius: 3,
+                                        color: Colors.white,
+                                        strokeWidth: 3,
+                                        strokeColor: secondary,
+                                      ),
+                                    ),
+                                  );
+                                }).toList();
+                              },
+                              touchTooltipData: LineTouchTooltipData(
+                                //tooltipBgColor: secondary,
+                                tooltipRoundedRadius: 20,
+                                getTooltipItems:
+                                    (List<LineBarSpot> lineBarsSpot) {
+                                  return lineBarsSpot.map((lineBarSpot) {
+                                    return LineTooltipItem(
+                                      "${lineBarSpot.x.toInt()} mins ago",
+                                      const TextStyle(
+                                        color: Colors.white,
+                                        fontSize: 10,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    );
+                                  }).toList();
+                                },
+                              ),
+                            ),
+                            lineBarsData: lineBarsData1,
+                            minY: -0.5,
+                            maxY: 110,
+                            titlesData: FlTitlesData(
+                                show: true,
+                                leftTitles: AxisTitles(),
+                                topTitles: AxisTitles(),
+                                bottomTitles: AxisTitles(
+                                  sideTitles: bottomTitles,
+                                ),
+                                rightTitles: AxisTitles(
+                                  sideTitles: rightTitles,
+                                )),
+                            gridData: FlGridData(
+                              show: true,
+                              drawHorizontalLine: true,
+                              horizontalInterval: 25,
+                              drawVerticalLine: false,
+                              getDrawingHorizontalLine: (value) {
+                                return FlLine(
+                                  color: Colors.white.withOpacity(0.15),
+                                  strokeWidth: 2,
+                                );
+                              },
+                            ),
+                            borderData: FlBorderData(
+                              show: true,
+                              border: Border.all(
+                                color: Colors.transparent,
+                              ),
+                            ),
+                          ),
+                        )),
+              ),
+            ];
+          },
+          body: 
+          // Estructura debajo de la gráfica
+          Container(
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: const BorderRadius.only(
+                topLeft: Radius.circular(25),
+                topRight: Radius.circular(25)
+              )
+            ),
+            child: Scaffold(
+              backgroundColor: Colors.transparent,
+            ),
+          )
+        ),
+    );
+  }
+
+  LineTouchData get lineTouchData1 => LineTouchData(
+        handleBuiltInTouches: true,
+        touchTooltipData: LineTouchTooltipData(
+            //tooltipBgColor: Colors.blueGrey.withOpacity(0.8),
+            ),
+      );
+
+  // Se crean dos señales en las gráfica
+  List<LineChartBarData> get lineBarsData1 => [
+        lineChartBarData1_1,
+        lineChartBarData1_2,
+      ];
+
+  // Señal 1
+  LineChartBarData get lineChartBarData1_1 => LineChartBarData(
+        isCurved: true,
+        color: Colors.white.withOpacity(0.5),
+        barWidth: 4,
+        isStrokeCapRound: true,
+        dotData: FlDotData(show: false),
+        belowBarData: BarAreaData(show: false),
+        spots: const [
+          FlSpot(1, 35),
+          FlSpot(2, 70),
+          FlSpot(3, 40),
+          FlSpot(4, 80),
+          FlSpot(5, 25),
+          FlSpot(6, 70),
+          FlSpot(7, 35),
+        ],
+      );
+
+  // Señal 2
+  LineChartBarData get lineChartBarData1_2 => LineChartBarData(
+        isCurved: true,
+        color: Colors.white,
+        barWidth: 2,
+        isStrokeCapRound: true,
+        dotData: FlDotData(show: false),
+        belowBarData: BarAreaData(
+          show: false,
+        ),
+        spots: const [
+          FlSpot(1, 80),
+          FlSpot(2, 50),
+          FlSpot(3, 90),
+          FlSpot(4, 40),
+          FlSpot(5, 80),
+          FlSpot(6, 35),
+          FlSpot(7, 60),
+        ],
+      );
+
+  SideTitles get rightTitles => SideTitles(
+        getTitlesWidget: rightTitleWidgets,
+        showTitles: true,
+        interval: 20,
+        reservedSize: 40,
+      );
+
+  Widget rightTitleWidgets(double value, TitleMeta meta) {
+    String text;
+    switch (value.toInt()) {
+      case 0:
+        text = '0%';
+        break;
+      case 20:
+        text = '20%';
+        break;
+      case 40:
+        text = '40%';
+        break;
+      case 60:
+        text = '60%';
+        break;
+      case 80:
+        text = '80%';
+        break;
+      case 100:
+        text = '100%';
+        break;
+      default:
+        return Container();
+    }
+
+    return Text(text,
+        style: TextStyle(
+          color: Colors.white,
+          fontSize: 12,
+        ),
+        textAlign: TextAlign.center);
+  }
+
+  SideTitles get bottomTitles => SideTitles(
+        showTitles: true,
+        reservedSize: 32,
+        interval: 1,
+        getTitlesWidget: bottomTitleWidgets,
+      );
+
+  Widget bottomTitleWidgets(double value, TitleMeta meta) {
+    var style = TextStyle(
+      color: Colors.white,
+      fontSize: 12,
+    );
+    Widget text;
+    switch (value.toInt()) {
+      case 1:
+        text = Text('Lun', style: style);
+        break;
+      case 2:
+        text = Text('Mar', style: style);
+        break;
+      case 3:
+        text = Text('Mie', style: style);
+        break;
+      case 4:
+        text = Text('Jue', style: style);
+        break;
+      case 5:
+        text = Text('Vie', style: style);
+        break;
+      case 6:
+        text = Text('Sab', style: style);
+        break;
+      case 7:
+        text = Text('Dom', style: style);
+        break;
+      default:
+        text = const Text('');
+        break;
+    }
+
+    return SideTitleWidget(
+      axisSide: meta.axisSide,
+      space: 10,
+      child: text,
     );
   }
 }
